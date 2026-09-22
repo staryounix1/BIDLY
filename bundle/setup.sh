4ef5394ba30c152684143026b36e24f7ada2e0ed
staryounix1 <younix.lox@gmail.com>
2026-09-21 20:16:36 -0500

    bundle v2026-09-22-8 (agree 42P08 fix)

--- bundle/setup.sh
+++ bundle/setup.sh
@@ -19,7 +19,7 @@ PARTS=91
 # Bump this whenever a new bundle is published. It is appended to every part
 # URL as a cache-buster so neither GitHub's raw CDN nor any intermediate cache
 # can hand back a stale part under the same filename.
-BUNDLE_VERSION="2026-09-22-7"
+BUNDLE_VERSION="2026-09-22-8"
 DIR="$HOME/bidly-api"
 PARTS_DIR="$DIR/parts"
 
